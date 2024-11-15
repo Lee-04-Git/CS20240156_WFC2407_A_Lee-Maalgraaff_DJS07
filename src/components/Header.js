744c876e27f71347a8c8c7d79c57bcedec7ce3b7
Lee-04-Git <leemaalgraaff.github@gmail.com>
2024-11-15 13:28:49 +0200

    added classNames to h tags

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,8 +4,8 @@ export default function Header() {
     return (
         <header className="header">
             <img src="./assets/troll-face.png" />
-            <h2>Header Component</h2>
-            <h4>React Course - Project 3</h4>
+            <h2 className="header--title">Header Component</h2>
+            <h4 className="header--project">React Course - Project 3</h4>
         </header>
     )
 }
