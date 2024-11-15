376107569868751c03811f1fefdc7f673719de59
Lee-04-Git <leemaalgraaff.github@gmail.com>
2024-11-15 13:23:27 +0200

    added header component with image and text for Project 3

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,1 +1,11 @@
-impo
+import React from "react";
+
+export default function Header() {
+    return (
+        <header className="header">
+            <img src="./assets/troll-face.png" />
+            <h2>Header Component</h2>
+            <h4>React Course - Project 3</h4>
+        </header>
+    )
+}
