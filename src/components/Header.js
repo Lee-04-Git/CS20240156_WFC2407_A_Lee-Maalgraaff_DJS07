74914bf71413897fdaa560b648aab15a279fc194
Lee-04-Git <leemaalgraaff.github@gmail.com>
2024-11-16 14:12:24 +0200

    added classNames for img and h2 tags

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,8 @@ import React from "react";
 export default function Header() {
     return (
         <header className="header">
-            <img src="./assets/troll-face.png" />
-            <h2 className="header--title">Header Component</h2>
+            <img className="header--image" src="./assets/troll-face.png" />
+            <h2 className="header--title">Meme Generator</h2>
             <h4 className="header--project">React Course - Project 3</h4>
         </header>
     )
